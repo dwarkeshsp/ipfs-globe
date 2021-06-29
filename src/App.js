a5d58e810fda1970f4515bba129474a572d1956e
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2021-06-29 10:39:10 -0500

    basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { ipstackKey } from "./keys";
+import Globe from "./Globe";
+
+const TypesEnum = {
+  SendingQuery: 0, //
+  PeerResponse: 1, // says use
+  FinalPeer: 2,
+  QueryError: 3,
+  Provider: 4,
+  Value: 5,
+  AddingPeer: 6,
+  DialingPeer: 7, // setting up connection
+};
 
 function App() {
+  useEffect(() => {
+    async function findProviders() {
+      const cid = "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR";
+      const url =
+        "https://node1.delegate.ipfs.io/api/v0/dht/findprovs?arg=" +
+        cid +
+        "&verbose=true";
+      // API seems to respond with multiple JSONs so I need to pull them together into an array
+      const response = await fetch(url, {
+        method: "POST",
+      });
+      const text = await response.text();
+      const responses = text
+        .split("\n")
+        .filter((line) => line.length > 0)
+        .map((line) => JSON.parse(line));
+      // for (const response of responses) {
+      //   if (response.Type === 4)
+      //     console.log(response.Responses[0], response.Type);
+      //   else console.log(response.ID, response.Type);
+      // }
+
+      return responses;
+    }
+
+    findProviders();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Globe />
     </div>
   );
 }

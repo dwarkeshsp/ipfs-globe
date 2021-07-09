34025cb9189f558be1afd2f25f843a1acd2b7056
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2021-07-09 17:47:14 -0500

    about to add provider arc data

--- src/Globe.js
+++ src/Globe.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import parseJson from "parse-json";
 import Globe from "react-globe.gl";
 import { ipstackKey } from "./keys";
@@ -42,7 +42,7 @@ async function getPeerData(ID) {
   return await getIPGeo(ip);
 }
 
-async function getUserData() {
+async function getUserGeo() {
   const url = "http://api.ipstack.com/check?access_key=" + ipstackKey;
   const response = await fetch(url);
   return parseJson(await response.text());
@@ -74,24 +74,25 @@ async function getArcData() {
   }
 
   const Responses = [
-    new Response("SendingQuery", "red", LARGE, ""),
-    new Response("PeerResponse", "yellow", LARGE, ""),
+    new Response("SendingQuery", "red", LARGE, "Sending query to"),
+    new Response("PeerResponse", "yellow", LARGE, "Recieving response from"),
     new Response("FinalPeer", "orange", LARGE, ""),
     new Response("QueryError", "black", LARGE, ""),
-    new Response("Provider", "green", 0.5, ""),
+    new Response("Provider", "green", 0.5, "Provider found"),
     new Response("Value", "violet", LARGE, ""),
     new Response("AddingPeer", "white", LARGE, ""),
-    new Response("DialingPeer", "blue", LARGE, ""),
+    new Response("DialingPeer", "blue", LARGE, "Dialing"),
   ];
 
   const providersData = await getProvidersData();
   console.log("providers Data", providersData);
-  const userData = await getUserData();
+  const userData = await getUserGeo();
 
   let arcsData = [];
-
-  for (const [i, response] of providersData.entries()) {
+  let i = 0;
+  for (const response of providersData) {
     const isProvider = Responses[response.Type].name === "Provider";
+    if (isProvider) continue;
     const peerID = isProvider ? response.Responses[0].ID : response.ID;
     const peerData = await getPeerData(peerID);
     if (peerData == null) continue;
@@ -103,7 +104,13 @@ async function getArcData() {
       endLat = peerData.latitude,
       endLng = peerData.longitude;
 
-    if (name === "PeerResponse") [startLat, startLng] = [endLat, endLng];
+    if (name === "PeerResponse")
+      [startLat, startLng, endLat, endLng] = [
+        endLat,
+        endLng,
+        startLat,
+        startLng,
+      ];
 
     arcsData.push({
       startLat: startLat,
@@ -112,9 +119,11 @@ async function getArcData() {
       endLng: endLng,
       color: color,
       dashGap: dashGap,
-      label: [labelPrefix, peerID.substring(0, 5), "..."].join(" "),
+      description: [labelPrefix, peerID.substring(0, 5), "..."].join(" "),
       initialGap: i,
     });
+
+    i++;
   }
 
   console.log("final arcs data", arcsData);
@@ -124,47 +133,67 @@ async function getArcData() {
 export default function GlobeWrapper() {
   const globeEl = useRef();
   const [arcsData, setArcsData] = useState([]);
+  const [description, setDescription] = useState("");
   // const startTime = useRef(new Date());
 
   useEffect(() => {
-    getArcData().then((data) => setArcsData(data));
-
     globeEl.current.controls().autoRotate = true;
-    globeEl.current.controls().autoRotateSpeed = 0.05;
+    globeEl.current.controls().autoRotateSpeed = 0.1;
+    getUserGeo().then(({ latitude, longitude }) =>
+      globeEl.current.pointOfView({ lat: latitude, lng: longitude }, 1000)
+    );
+
+    // getArcData().then((data) => setArcsData(data));
   }, []);
 
   useEffect(() => {
     arcsData.length && console.log("arcsData", arcsData);
-    if (arcsData.length) {
-      const { startLat, startLng } = arcsData[0];
-      const FOCUSTIME = 1000;
-      globeEl.current.pointOfView({ lat: startLat, lng: startLng }, FOCUSTIME);
-
-      // const endTime = new Date();
-      // const delay = (endTime - startTime.current + 2 * FOCUSTIME) / 1000;
-      const delay = 2;
-
-      console.log("delay (s):", delay);
-      for (let i = 0; i < arcsData.length; i++) {
-        arcsData[i].initialGap += delay;
-      }
+    if (!arcsData.length) {
+      return;
     }
+    const FOCUSTIME = 1000;
+
+    let index = 0;
+
+    const interval = setInterval(() => {
+      if (index < arcsData.length) {
+        setDescription(arcsData[index].description);
+        index++;
+      } else {
+        setDescription("");
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+
+    // const endTime = new Date();
+    // const delay = (endTime - startTime.current + 2 * FOCUSTIME) / 1000;
+    // const delay = 1;
+
+    // console.log("delay (s):", delay);
+    // for (let i = 0; i < arcsData.length; i++) {
+    //   arcsData[i].initialGap += delay;
+    // }
   }, [arcsData]);
 
   return (
-    <Globe
-      // height={window.innerHeight * 0.8}
-      ref={globeEl}
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      arcsData={arcsData}
-      arcColor={(d) => d.color}
-      arcLabel={(d) => d.label}
-      arcDashLength={0.5}
-      arcDashGap={10000}
-      arcDashInitialGap={(d) => d.initialGap}
-      arcDashAnimateTime={1000}
-      // TODO
-      // onArcHover={() => null}
-    />
+    <React.Fragment>
+      {/* <div style={{ height: "10vh"}}>
+        <h3>{description}</h3>
+      </div> */}
+      <Globe
+        // height={window.innerHeight * 0.9}
+        ref={globeEl}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        arcsData={arcsData}
+        arcColor={(d) => d.color}
+        arcLabel={(d) => d.label}
+        arcDashLength={0.5}
+        arcDashGap={10000}
+        arcDashInitialGap={(d) => d.initialGap}
+        arcDashAnimateTime={1000}
+        // TODO
+        // onArcHover={() => null}
+      />
+    </React.Fragment>
   );
 }
